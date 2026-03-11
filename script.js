b219b50b58c6875643a0746a10e6489936ccfa6b
Vishal Yadav <ydvishal89@gmail.com>
2026-03-11 18:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Person {
 	constructor(name , age){
-		this.name = name;
-		this.age= age;
+		this._name = name;
+		this._age = age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
 	set age(age){
-		this.age = age;
+		this._age = age;
 	}
 	
 }
